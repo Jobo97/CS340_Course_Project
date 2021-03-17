7c6cd306dc39b695b0284a009a2fcbf3f30674b0
Jobo97 <ben.c.millett@gmail.com>
2021-03-16 21:31:59 -0600

    Request updates

--- shared/src/main/java/com/example/shared/src/main/java/edu/byu/cs/tweeter/model/service/request/StatusRequest.java
+++ shared/src/main/java/com/example/shared/src/main/java/edu/byu/cs/tweeter/model/service/request/StatusRequest.java
@@ -1,10 +1,10 @@
 package com.example.shared.src.main.java.edu.byu.cs.tweeter.model.service.request;
 
 public class StatusRequest {
-    private final String userAlias;
-    private final int limit;
-    private final String lastStatus;
-    private final boolean isStory;
+    private String userAlias;
+    private int limit;
+    private String lastStatus;
+    private boolean isStory;
 
 
     public StatusRequest(String userAlias, int limit, String lastStatus, boolean isStory) {
@@ -12,6 +12,9 @@ public class StatusRequest {
         this.limit = limit;
         this.lastStatus = lastStatus;
         this.isStory = isStory;
+    }
+
+    public StatusRequest() {
     }
 
     public String getLastStatus() {
@@ -28,5 +31,21 @@ public class StatusRequest {
 
     public boolean isStory() {
         return isStory;
+    }
+
+    public void setUserAlias(String userAlias) {
+        this.userAlias = userAlias;
+    }
+
+    public void setLimit(int limit) {
+        this.limit = limit;
+    }
+
+    public void setLastStatus(String lastStatus) {
+        this.lastStatus = lastStatus;
+    }
+
+    public void setStory(boolean story) {
+        isStory = story;
     }
 }
--- shared/src/main/java/com/example/shared/src/main/java/edu/byu/cs/tweeter/model/service/request/UserFollowRequest.java
+++ shared/src/main/java/com/example/shared/src/main/java/edu/byu/cs/tweeter/model/service/request/UserFollowRequest.java
@@ -1,12 +1,15 @@
 package com.example.shared.src.main.java.edu.byu.cs.tweeter.model.service.request;
 
 public class UserFollowRequest {
-    private final String userAlias;
-    private final String viewedAlias;
+    private String userAlias;
+    private String viewedAlias;
 
     public UserFollowRequest(String userAlias, String viewedAlias) {
         this.userAlias = userAlias;
         this.viewedAlias = viewedAlias;
+    }
+
+    public UserFollowRequest() {
     }
 
     public String getUserAlias() {
@@ -15,5 +18,13 @@ public class UserFollowRequest {
 
     public String getViewedAlias() {
         return viewedAlias;
+    }
+
+    public void setUserAlias(String userAlias) {
+        this.userAlias = userAlias;
+    }
+
+    public void setViewedAlias(String viewedAlias) {
+        this.viewedAlias = viewedAlias;
     }
 }
